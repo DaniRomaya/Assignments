48712f1c47423b87a01832153b335fc95278ba6d
Dani Romaya <DaniRomaya>
2024-01-20 12:21:06 -0500

    first commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+        int a= 21;
+        if (a<22);
+            System.out.println("Dani");
+        System.out.println("Hello, my name is dani");   
+    }
+}
